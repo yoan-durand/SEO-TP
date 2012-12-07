cf7b70336193751556e1ad2e81ab7a4d28dd0d2e
yoan <durand.yoan@gmail.com>
2012-12-07 10:41:18 +0100

    add urlloading

---
+++ ComparePage/src/urlloading/LoadUrl.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package urlloading;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author yoan
+ */
+public class LoadUrl 
+{
+    public LoadUrl()
+    {
+        
+    }
+    
+    public static String loadUrl(URL url) throws IOException 
+    {
+        InputStream stream = null;
+        try 
+        {
+            stream = url.openStream();
+            return loadStream(stream);
+        } 
+        finally 
+        {
+            if (stream != null) 
+            {
+                try 
+                {
+                    stream.close();
+                } 
+                catch (IOException e) 
+                {
+                }
+            }
+        }
+    }
+    
+     public static String loadStream(InputStream stream) throws IOException 
+     {
+        Reader reader = new InputStreamReader(stream, Charset.forName("UTF-8"));
+        char[] buffer = new char[1024];
+        int count;
+        StringBuilder str = new StringBuilder();
+        while ((count = reader.read(buffer)) != -1)
+        {
+            str.append(buffer, 0, count);
+        }
+        return str.toString();
+     }
+}
